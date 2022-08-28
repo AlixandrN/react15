b73eb0db4a1b31ecd89f499ae584d6fa15b36b24
Alexandr <nasanovicha@gmail.com>
2022-08-28 22:50:47 +0300

    feat: add useEffect hook

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,16 +6,23 @@ import Input1 from './components/UI/Input1';
 
 function App() {
 const [disable, setDisable] = useState(false);
+// const [customVal, setCustomVal] = useState(0);
 
 const inputReference = useRef();
 
-// useEffect(() => {
-//   inputReference.current.focus();
-// }, []);
+
+useEffect(() => {inputReference.current.focus();}, []);
+useEffect(() => {
+  console.log('the button status has been changed')
+  return () => {console.log('test after')}
+}, [disable]);
+
+
 
 const focus = (e) => {
   e.preventDefault();
   inputReference.current.focus();
+  // inputReference.current.value = 55
 }
 
 const isBtnDisabled = (disStat) => setDisable(disStat)
@@ -25,6 +32,7 @@ const isBtnDisabled = (disStat) => setDisable(disStat)
 
       <form className='form'>  
       <Input1 func={isBtnDisabled} ref={inputReference} />  
+
       <Button1 status={disable} > Push </Button1>
       <Button1 onClick={focus} > focus </Button1>
       </form>
